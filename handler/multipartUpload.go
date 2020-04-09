ece8cec6d7b0055f0e5d93af8e22742be3b7d334
curder <curder@foxmail.com>
2020-04-09 19:12:21 +0800

    分块上传

--- handler/multipartUpload.go
+++ handler/multipartUpload.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+    "fmt"
     "github.com/curder/file-store-server/utils"
     "math"
     "net/http"
+    "os"
     "strconv"
     "time"
 
@@ -53,4 +55,41 @@ func InitiateMultipartUploadHandler(w http.ResponseWriter, r *http.Request) {
 
     // 将响应初始化数据返回给客户端
     w.Write(utils.NewResponse(0, "Successful", multipartInfo).JSONBytes())
+}
+
+// 分块上传
+func UploadPartHandler(w http.ResponseWriter, r *http.Request) {
+    // 解析用户请求参数
+    r.ParseForm()
+
+    uploadID := r.Form.Get("upload_id")     // 上传的id
+    chunkIndex := r.Form.Get("chunk_index") // 分块索引
+
+    // 获得Redis连接
+    redisConnection := redisPool.RedisPool().Get()
+    defer redisConnection.Close()
+
+    // 获得文件句柄，用于存储分块内容
+    fd, err := os.Create("/tmp/go-test-files/multipart-uploads/" + uploadID + "/" + chunkIndex)
+    if err != nil {
+        fmt.Printf(err.Error())
+        w.Write(utils.NewResponse(-1, "Upload part failed", nil).JSONBytes())
+    }
+    defer fd.Close()
+
+    // 每次读取 1M
+    buffer := make([]byte, 1024*1024)
+    for {
+        n, err := r.Body.Read(buffer)
+        fd.Write(buffer[:n])
+        if err != nil {
+            break
+        }
+    }
+    // 更新redis缓存状态
+    _, _ = redisConnection.Do("HSET", "multipart_upload:"+uploadID, "chunk_index:"+chunkIndex, 1)
+
+    // 返回处理结果到客户端
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(utils.NewResponse(0, "Successful", nil).JSONBytes())
 }
